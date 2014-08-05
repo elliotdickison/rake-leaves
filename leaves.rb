11eb51ff126f48e90d42737f6ca85681a5c2fc9a
Elliot Dickison <ejdickison@gmail.com>
2014-08-04 20:44:15 -0700

    Added the ability to specify whether missing parameters should be requested from the user... if the feature is turned off then missing parameters will cause errors to be thrown.

--- leaves.rb
+++ leaves.rb
@@ -20,10 +20,16 @@ module Rake
 			aliases[original_name].push new_name
 			Rake.application.last_param_aliases = aliases
 		end
+
+		# Allow the task to request missing arguments from the user
+		def request_missing_params
+			Rake.application.last_request_missing_params = true
+		end
 	end
 
 	class Task
 
+		attr_writer :request_missing_params
 		alias_method :original_invoke_with_call_chain, :invoke_with_call_chain
 
 		# Call self.args in self.invoke_with_call_chain so that all the arg
@@ -36,17 +42,35 @@ module Rake
 		# Gathers and returns all args for the task. Missing required args are
 		# requested via standard in
 		def args
-			@args ||= required_params
-				.merge(optional_params)
-				.inject({}) do |args, (name, default)|
-					param_aliases[name].reverse.each { |alias_name|
-						args[name] ||= ENV[alias_name.to_s]
-					} if param_aliases[name]
-					args[name] ||= ENV[name.to_s] || default
-					args[name] = request_argument name if
-						args[name].nil? && required_params.keys.include?(name)
-					args
-				end
+			unless @args
+				errors = []
+				@args = required_params
+					.merge(optional_params)
+					.inject({}) do |args, (name, default)|
+
+						# Check for a value supplied under an alias
+						param_aliases[name].reverse.each { |alias_name|
+							args[name] ||= ENV[alias_name.to_s]
+						} if param_aliases[name]
+
+						# Set the arg to user input or its default value
+						args[name] ||= ENV[name.to_s] || default
+
+						# If an argument is a) missing and b) required, then
+						# request it or generate an error message
+						if args[name].nil? && required_params.keys.include?(name)
+							if @request_missing_params
+								args[name] = request_argument name 
+							else
+								errors.push "Missing argument '#{name}'"
+							end
+						end
+
+						args
+					end
+				abort errors.join "\n" unless errors.empty?
+			end
+			@args
 		end
 
 		# Add a required argument to the task
@@ -87,13 +111,14 @@ module Rake
 
 		# Request an argument from the user via standard in
 		def request_argument  (name)
-			STDOUT.print "#{@name} requires #{name}: "
+			STDOUT.print "Enter #{name} for #{@name}: "
 			ENV[name.to_s] = STDIN.gets.strip
 		end
 	end
 
 	module TaskManager
-		attr_writer :last_required_params, :last_optional_params, :last_param_aliases
+		attr_writer :last_required_params, :last_optional_params, :last_param_aliases,
+			:last_request_missing_params
 		
 		alias_method :original_intern, :intern
 
@@ -138,15 +163,17 @@ module Rake
 					task.add_param_alias new_name, original_name
 				}
 			}
+			task.request_missing_params = @last_request_missing_params
 			@last_required_params = nil
 			@last_optional_params = nil
 			@last_param_aliases = nil
+			@last_request_missing_params = nil
 		end
 	end
 
 	module DSL
 
-		# Include the new methods that should accessible globally
+		# Include the new methods that should be accessible globally
 		include Leaves
 		private(*Leaves.instance_methods(false))
 	end
